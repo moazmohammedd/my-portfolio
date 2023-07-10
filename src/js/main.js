6051936a5915f047c4dcfaaf29c302a809ee89b5
moazmohammedd <moazsonic999@gmail.com>
2023-07-11 01:28:09 +0300

    add landing

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-var i = 0; 
+const socailBtn = document.getElementById('socail-btn');
+let socailMenu  = document.querySelector('.social-menu')
+if(socailBtn){
+    socailBtn.addEventListener("click" , (e)=>{
+        if(socailMenu){
+            socailMenu.classList.toggle('ac')
+        }
+        e.target.classList.toggle('ac')
+        
+    })
+}
