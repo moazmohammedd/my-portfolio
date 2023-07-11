13222f92ad48279f866a447db51a606a93b05ec8
moazmohammedd <moazsonic999@gmail.com>
2023-07-11 16:08:31 +0300

    add an animation to the button after page loding

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,35 @@
-const socailBtn = document.getElementById('socail-btn');
-let socailMenu  = document.querySelector('.social-menu')
-if(socailBtn){
-    socailBtn.addEventListener("click" , (e)=>{
-        if(socailMenu){
-            socailMenu.classList.toggle('ac')
-        }
-        e.target.classList.toggle('ac')
-        
-    })
+const socailBtn = document.getElementById("socail-btn");
+let socailMenu = document.querySelector(".social-menu");
+if (socailBtn) {
+  var btnTxt = ["My Scail Links", "Close Menu"];
+  socailBtn.addEventListener("click", (e) => {
+    if (socailMenu) {
+      socailMenu.classList.toggle("ac");
+    }
+    e.target.classList.toggle("ac");
+    if (e.target.classList.contains("ac")) {
+      e.target.innerHTML = `${btnTxt[1]}`;
+    } else {
+      e.target.innerHTML = `${btnTxt[0]}`;
+    }
+  });
 }
+
+const newspaperSpinning = [
+  { transform: "rotate(8deg)" ,opacity:"0.5"},
+  { transform: "rotate(-8deg)" },
+  { transform: "rotate(8deg)" },
+  { transform: "rotate(-8deg)" },
+  { transform: "rotate(8deg)" ,opacity:"1"},
+  { transform: "rotate(-8deg)" },
+  { transform: "rotate(0deg)" },
+];
+
+const newspaperTiming = {
+  duration: 500,
+  iterations: 1,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  socailBtn.animate(newspaperSpinning, newspaperTiming);;
+});
